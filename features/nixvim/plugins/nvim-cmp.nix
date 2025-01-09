8c6d74c31043cd942dfcd0aac65ae9deef5367d8
Lionel Karlen <lionel.karlen@protonmail.com>
2025-01-09 04:56:20 +0100

    fix < delay

--- features/nixvim/plugins/nvim-cmp.nix
+++ features/nixvim/plugins/nvim-cmp.nix
@@ -1,91 +1,89 @@
 {
-	programs.nixvim = {
-		plugins.luasnip.enable=true;
-		plugins.cmp-nvim-lsp.enable=true;
-		plugins.cmp-path.enable=true;
-		extraLuaPackages = ps: [
-			ps.jsregexp
-		];
+  programs.nixvim = {
+    plugins.luasnip.enable = true;
+    plugins.cmp-nvim-lsp.enable = true;
+    plugins.cmp-path.enable = true;
+    extraLuaPackages = ps: [
+      ps.jsregexp
+    ];
 
+    plugins.cmp = {
+      enable = true;
+      settings = {
+        snippet = {
+          expand = ''
+            function(args)
+            	require('luasnip').lsp_expand(args.body)
+            end
+          '';
+        };
 
-		plugins.cmp = {
-			enable=true;
-			settings = {
-				snippet= {
-					expand = ''
-					function(args)
-						require('luasnip').lsp_expand(args.body)
-					end
-					'';
-				};
+        completion = {
+          completeopt = "menu,menuone,noinsert";
+        };
 
-				completion = {
-					completeopt="menu,menuone,noinsert";
-				};
+        mapping = {
+          "<C-n>" = "cmp.mapping.select_next_item()";
+          "<C-p>" = "cmp.mapping.select_prev_item()";
+          "<C-j>" = "cmp.mapping.select_next_item()";
+          "<C-k>" = "cmp.mapping.select_prev_item()";
+          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          # Accept ([y]es) the completion.
+          #  This will auto-import if your LSP supports it.
+          #  This will expand snippets if the LSP sent a snippet.
+          # "<C-y>" = "cmp.mapping.confirm { select = true }";
+          # If you prefer more traditional completion keymaps,
+          # you can uncomment the following lines.
+          "<CR>" = "cmp.mapping.confirm { select = true }";
+          "<Tab>" = "cmp.mapping.confirm { select = true }";
 
-				mapping = {
-					"<C-n>" = "cmp.mapping.select_next_item()";
-					"<C-p>" = "cmp.mapping.select_prev_item()";
-					"<C-j>" = "cmp.mapping.select_next_item()";
-					"<C-k>" = "cmp.mapping.select_prev_item()";
-					"<C-b>" = "cmp.mapping.scroll_docs(-4)";
-					"<C-f>" = "cmp.mapping.scroll_docs(4)";
-					# Accept ([y]es) the completion.
-					#  This will auto-import if your LSP supports it.
-					#  This will expand snippets if the LSP sent a snippet.
-					# "<C-y>" = "cmp.mapping.confirm { select = true }";
-					# If you prefer more traditional completion keymaps,
-					# you can uncomment the following lines.
-					"<CR>" = "cmp.mapping.confirm { select = true }";
-					"<Tab>" = "cmp.mapping.confirm { select = true }";
-		
-					# Manually trigger a completion from nvim-cmp.
-					#  Generally you don't need this, because nvim-cmp will display
-					#  completions whenever it has completion options available.
-					"<C-Space>" = "cmp.mapping.complete {}";
-					"<C-.>" = "cmp.mapping.complete {}";
-		
-					# Think of <c-l> as moving to the right of your snippet expansion.
-					#  So if you have a snippet that's like:
-					#  function $name($args)
-					#    $body
-					#  end
-					#
-					# <c-l> will move you to the right of the expansion locations.
-					# <c-h> is similar, except moving you backwards.
-					"<C-l>" = ''
-					  cmp.mapping(function()
-					    if luasnip.expand_or_locally_jumpable() then
-					      luasnip.expand_or_jump()
-					    end
-					  end, { 'i', 's' })
-					'';
-					"<C-h>" = ''
-					  cmp.mapping(function()
-					    if luasnip.locally_jumpable(-1) then
-					      luasnip.jump(-1)
-					    end
-					  end, { 'i', 's' })
-					'';
-		
-					# For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-					#    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-				};
+          # Manually trigger a completion from nvim-cmp.
+          #  Generally you don't need this, because nvim-cmp will display
+          #  completions whenever it has completion options available.
+          "<C-Space>" = "cmp.mapping.complete {}";
 
-				autoEnableSources=true;
-				
-				sources = [
-					{
-						name="luasnip";
-					}
-					{
-						name="nvim_lsp";
-					}
-					{
-						name="path";
-					}
-				];
-			};
-		};
-	};
+          # Think of <c-l> as moving to the right of your snippet expansion.
+          #  So if you have a snippet that's like:
+          #  function $name($args)
+          #    $body
+          #  end
+          #
+          # <c-l> will move you to the right of the expansion locations.
+          # <c-h> is similar, except moving you backwards.
+          "<C-l>" = ''
+            cmp.mapping(function()
+              if luasnip.expand_or_locally_jumpable() then
+                luasnip.expand_or_jump()
+              end
+            end, { 'i', 's' })
+          '';
+          "<C-h>" = ''
+            cmp.mapping(function()
+              if luasnip.locally_jumpable(-1) then
+                luasnip.jump(-1)
+              end
+            end, { 'i', 's' })
+          '';
+
+          # For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+          #    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        };
+
+        autoEnableSources = true;
+
+        sources = [
+          {
+            name = "luasnip";
+          }
+          {
+            name = "nvim_lsp";
+          }
+          {
+            name = "path";
+          }
+        ];
+      };
+    };
+  };
 }
