7710b4f1738d6280100478c4379a4c475ec7a36e
Lionel Karlen <lionel.karlen@protonmail.com>
2026-01-19 17:13:03 +0100

    fix(helix): fix yazi opening with unescaped filepaths

--- shared/helix/plugins.nix
+++ shared/helix/plugins.nix
@@ -4,7 +4,7 @@ lib.mkIf config.yazi.enable {
   programs.helix.settings.keys.normal.space = {
     "e" = [
       ":sh rm -f /tmp/yazi-unique-file"
-      ":insert-output yazi %{buffer_name} --chooser-file=/tmp/yazi-unique-file"
+      '':insert-output yazi "%{buffer_name}" --chooser-file=/tmp/yazi-unique-file''
       '':sh printf "\x1b[?1049h\x1b[?2004h" > /dev/tty''
       ":open %sh{cat /tmp/yazi-unique-file}"
       ":redraw"
